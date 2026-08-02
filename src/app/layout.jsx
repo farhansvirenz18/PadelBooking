3bdd677db9490f0115309597defe29cf5be13dee
FarhanVirenze <126660105+FarhanVirenze@users.noreply.github.com>
2026-08-02 09:26:21 +0700

    update

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -14,6 +14,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="id" className={`${sora.variable} ${hanken.variable}`}>
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@24,400,0,0" rel="stylesheet" />
+      </head>
       <body className="antialiased">
         {children}
         <Toaster position="top-right" richColors />
