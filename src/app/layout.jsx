1e6eed3d0560f0b583d7fefa3a0b6c5f027b2647
FarhanVirenze <126660105+FarhanVirenze@users.noreply.github.com>
2026-08-04 10:07:58 +0700

    update

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,6 +1,7 @@
 import { Bebas_Neue, Space_Grotesk, DM_Sans } from 'next/font/google';
 import './globals.css';
 import { Toaster } from 'sonner';
+import Script from 'next/script';
 
 const bebas = Bebas_Neue({ subsets: ['latin'], variable: '--font-brand', weight: '400', display: 'swap' });
 const spaceGrotesk = Space_Grotesk({ subsets: ['latin'], variable: '--font-display', display: 'swap' });
@@ -23,16 +24,17 @@ export default function RootLayout({ children }) {
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" rel="stylesheet" />
         <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@24,400,0,0" rel="stylesheet" />
-        <script
-          src={process.env.NEXT_PUBLIC_MIDTRANS_IS_PRODUCTION === 'true'
-            ? 'https://app.midtrans.com/snap/snap.js'
-            : 'https://app.sandbox.midtrans.com/snap/snap.js'}
-          data-client-key={process.env.NEXT_PUBLIC_MIDTRANS_CLIENT_KEY || ''}
-        />
       </head>
       <body className="antialiased">
         {children}
         <Toaster position="top-right" richColors closeButton className="z-[999]!" />
+        <Script
+          src={process.env.NEXT_PUBLIC_MIDTRANS_IS_PRODUCTION === 'true'
+            ? 'https://app.midtrans.com/snap/snap.js'
+            : 'https://app.sandbox.midtrans.com/snap/snap.js'}
+          data-client-key={process.env.NEXT_PUBLIC_MIDTRANS_CLIENT_KEY || ''}
+          strategy="beforeInteractive"
+        />
       </body>
     </html>
   );
